38da7795b35473ed13f215904360ff0c699bb6ea
kaito0228 <aqua.timez.saiko@gmail.com>
2020-10-25 00:22:52 +0900

    implements function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,35 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Cloud Firestore under the path /messages/:documentId/original
+export const addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
+
+export const makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+        // Grab the current value of what was written to Cloud Firestore.
+        const original = snap.data().original;
+
+        // Access the parameter `{documentId}` with `context.params`
+        functions.logger.log('Uppercasing', context.params.documentId, original);
+
+        const uppercase = original.toUpperCase();
+
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+        // writing to Cloud Firestore.
+        // Setting an 'uppercase' field in Cloud Firestore document returns a Promise.
+        return snap.ref.set({uppercase}, {merge: true});
+    });
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
